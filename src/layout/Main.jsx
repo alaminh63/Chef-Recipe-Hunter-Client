5669b5911cf9518fc2e835403b2b1e32d623259e
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-05-03 03:23:32 +0600

    banner image

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Home from "../pages/Home/Home/Home";
 import Header from "../pages/shared/Header/Header";
 import NavigationBar from "../pages/shared/NavigationBar/NavigationBar";
 
 const Main = () => {
   return (
     <div>
-      <Header></Header>
       <NavigationBar></NavigationBar>
       <Outlet></Outlet>
+      <Home></Home>
     </div>
   );
 };
---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <div class="h-56  bg-gradient-to-r from-transparent to-slate-800"></div>
+      
+    </div>
+  );
+};
+
+export default Banner;
