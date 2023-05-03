96fac8e39a9e2f1d2713c5e67a793f1a02e24a7a
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-05-03 09:53:59 +0600

    folder structure done

---
+++ src/Contexts/AuthContexts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthContexts = () => {
+    return (
+        <div>
+            <h2>Auth Contexts</h2>
+        </div>
+    );
+};
+
+export default AuthContexts;
